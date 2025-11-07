07652cf18e58e20ee229bb0a50ffa69b4c16b099
pythonplayer396 <traprian095@gmail.com>
2025-11-06 23:48:36 -0500

    dfd

--- app/api/applications/status/route.ts
+++ app/api/applications/status/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { getStore } from '@netlify/blobs'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
   try {
     const session = await getServerSession()
